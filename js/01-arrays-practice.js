d13c6fc6a860c4e227580200934b318881179ff4
Jose Pereda <joselpereda@hotmail.com>
2021-04-11 16:21:45 -0400

    Initial commit

---
+++ js/01-arrays-practice.js
@@ -0,0 +1,45 @@
+//STEP 1
+// Create a string array that contains your five favorite movies. Then, use the console to display the second movie in your array.
+// let arrayMyFavoriteMovies = ['Casino', 'Braveheart', 'Good Fellas', 'The Matrix', 'Heat', 'Scicario'];
+// console.log(arrayMyFavoriteMovies[1]);
+
+//STEP 2
+// Declare an array called movies using the function constructor method. Add the length of 5 into the constructor. Then, assign one of your favorite movies to each index in the array until you have 5 total movies in your array. Then, use the console to display the first movie in your array.
+// let Movies = new Array(5);
+// Movies[0] = 'Casino'; 
+// Movies[1] = 'Braveheart';
+// Movies[2] = 'Good Fellas';
+// Movies[3] = 'The Matrix';
+// Movies[4] = 'Heat';
+
+// console.log(Movies[0]);
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
